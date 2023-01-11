c3da399741898fc955e0811ca56f0ab2a6270bcd
pradeep.padmukhi <pradeep.padmukhi@vivaconnect.co>
2023-01-11 15:38:21 +0530

    real-time streaming with Web Socket

---
+++ real_time_dataStreaming_nodejs/web_sockets/app.js
@@ -0,0 +1,41 @@
+const http = require('http')
+const express = require('express')
+const WebSocket = require('ws')
+const app = express()
+const port = 3002
+
+// app.use('/', express.static('public'));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server })
+
+var data = "Real-Time Update 1";
+var number = 1;
+
+wss.on('connection', ws => {
+
+    ws.on('message', message => {
+        console.log(`Received message => ${message}`)
+    })
+
+    var interval = setInterval(function () {
+        data = "Real-Time Update " + number;
+        console.log("SENT: " + data);
+        ws.send(data)
+        number++;
+    }, randomInteger(1, 5) * 1000);
+
+    ws.on('close', function close() {
+        clearInterval(interval);
+    });
+})
+
+function randomInteger(min, max) {
+    let time = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log("time: ", time);
+    return time
+}
+
+server.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
