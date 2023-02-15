3d31c01c3c83ebc3cd51228122dfe7e4cc51b452
pradeep.padmukhi <pradeep.padmukhi@vivaconnect.co>
2023-02-15 19:23:43 +0530

    obj of arr set to key name

--- node_with_redis/redisConnection.js
+++ node_with_redis/redisConnection.js
@@ -3,16 +3,26 @@ const { createClient } = require('redis');
 const client = createClient();
 
 
-(async (key, value) => {
+const redisOP = async (key, value) => {
     await client.connect();
     console.log("connected"
     )
     await client.set(key, value);
     const val = await client.get(key);
-    console.log("get val", val)
+    let objval = JSON.parse(val)
+    console.log("get val", objval)
 
-    await client.hSet('frameworks_hash', {'javascript':'ReactJS', 'css':'TailwindCSS', 'node':'Express'});
-    let hval = await client.hGetAll('frameworks_hash');
+    await client.hSet('frameworks_hash', {'javascript':'ReactJS', 'css':'TailwindCSS', 'node':'Express', 'AI':'Python'});
+    let hvalall = await client.hGetAll('frameworks_hash');
+    let hval = await client.hGet('frameworks_hash', 'node');
+
+    console.log("hval get all", hvalall)
     console.log("hval get all", hval)
     await client.disconnect();
-})('name', 'pradeep')
+}
+
+let obj =  [{'name':'pradeep', 'role':'wevdev', 'city':'mumbai'}, {'name':'rahul', 'role':'python', 'city':'mumbai'}]
+let str = JSON.stringify(obj)
+console.log('str----', str);
+
+redisOP('name', str)
