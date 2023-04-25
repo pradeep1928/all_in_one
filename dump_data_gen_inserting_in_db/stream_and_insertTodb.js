407f277332f86f21c38ad796f41528ee73850528
pradeep.padmukhi <pradeep.padmukhi@vivaconnect.co>
2023-04-25 15:59:08 +0530

    js-doc added to steam file

--- dump_data_gen_inserting_in_db/stream_and_insertTodb.js
+++ dump_data_gen_inserting_in_db/stream_and_insertTodb.js
@@ -13,6 +13,13 @@ const db_config1 = {
 let conn_test_1 = mysql.createPool(db_config1);
 
 
+/**
+ * The function inserts data into a MySQL database table called "camp_mobile_list".
+ * @param data - The `data` parameter is an array of arrays, where each inner array represents a row of
+ * data to be inserted into the `camp_mobile_list` table. The inner arrays should contain values for
+ * the `mobile`, `var1`, and `var2` columns in that order.
+ * @returns The function `insertDataMysql` is returning a Promise object.
+ */
 const insertDataMysql = (data) => {
     return new Promise((resolve, reject) => {
         let query_camp_mobile_list = `insert into camp_mobile_list (mobile, var1, var2) VALUES ? `
@@ -27,7 +34,11 @@ const insertDataMysql = (data) => {
 }
 
 
-async function fileReading() {
+/**
+ * This function reads data from an Excel file, processes it in batches of 1000 rows, and inserts it
+ * into a MySQL database.
+ */
+async function xlsx_fileReading_1() {
     var file_path = `/home/viva/Desktop/pradeep/All_in _one/all_in_one/dump_data_gen_inserting_in_db/upload/add_contacts_new.xlsx`
     const stream = await getXlsxStream({
         filePath: file_path,
@@ -36,25 +47,28 @@ async function fileReading() {
     let dataArr = []
     stream.on('data', async (exlrow) => {
         let exlArr = exlrow.raw.arr;
+        console.log("🚀 ~ file: stream_and_insertTodb.js:39 ~ stream.on ~ exlArr:", exlArr)
         dataArr.push(exlArr);
     })
 
     stream.on('end', async () => {
         console.log("in end process ------->");
         let smallArr = []
-        for (let i = 1; i < dataArr.length; i++) {
-            smallArr.push(dataArr[i])
-            if (smallArr.length == 1000) {
-                console.log("🚀 ~ file: test.js:49 ~ stream.on ~ smallArr:", smallArr.length)
-                let res = await insertDataMysql(smallArr)
-                console.log("🚀 ~ file: test.js:41 ~ stream.on ~ res:", res)
-                smallArr = []
+        if (dataArr.length) {
+            for (let i = 1; i < dataArr.length; i++) {
+                smallArr.push(dataArr[i])
+                if (smallArr.length == 1000) {
+                    console.log("🚀 ~ file: test.js:49 ~ stream.on ~ smallArr:", smallArr.length)
+                    let res = await insertDataMysql(smallArr)
+                    console.log("🚀 ~ file: test.js:41 ~ stream.on ~ res:", res)
+                    smallArr = []
+                }
+        
             }
-    
-        }
-        if (smallArr.length > 0) {
-            let res = await insertDataMysql(smallArr)
-            console.log("🚀 ~ file: test.js:58 ~ stream.on ~ res:", res)
+            if (smallArr.length > 0) {
+                let res = await insertDataMysql(smallArr)
+                console.log("🚀 ~ file: test.js:58 ~ stream.on ~ res:", res)
+            }    
         }
         console.log("end of process ------------> ");
         dataArr = []
@@ -63,6 +77,4 @@ async function fileReading() {
 }
 
 
-
-
-fileReading()
+xlsx_fileReading_1()
