762f436a9346dc3e956f858f161cd04c7d1130f2
pradeep.padmukhi <pradeep.padmukhi@vivaconnect.co>
2023-07-20 19:06:31 +0530

    csv file reading in stream

--- dump_data_gen_inserting_in_db/stream_and_insertTodb.js
+++ dump_data_gen_inserting_in_db/stream_and_insertTodb.js
@@ -2,6 +2,8 @@ const { getXlsxStream } = require("xlstream");
 const xlsx = require('xlsx');
 const mysql = require("mysql2");
 const excelToJson = require("convert-excel-to-json");
+const fs = require('fs');
+const csv = require('csv-parser');
 const Transform = require('stream').Transform
 
 const db_config1 = {
@@ -45,18 +47,27 @@ async function xlsx_fileReading() {
     });
     let flag = false;
     let dataArr = []
+    let chunk_index = 0;
+    let last_chunk_index = 0
     stream.on('data', async (exlrow) => {
         try {
             let exlArr = exlrow.raw.arr;
             if (flag) {
                 dataArr.push(exlArr);
                 if (dataArr.length == 1000) {
-                    stream.pause()
-                    console.log("🚀 ~ file: stream_and_insertTodb.js:52 ~ stream.on ~ dataArr.length:", dataArr.length)
-                    let res = await insertDataMysql(dataArr)
-                    console.log("🚀 ~ file: stream_and_insertTodb.js:53 ~ stream.on ~ res:", res.info)
+                    if (parseInt(chunk_index) >= parseInt(last_chunk_index)) {
+                        stream.pause()
+                        console.log("🚀 ~ file: stream_and_insertTodb.js:52 ~ stream.on ~ dataArr.length:", dataArr.length)
+                        let res = await insertDataMysql(dataArr)
+                        console.log("🚀 ~ file: stream_and_insertTodb.js:53 ~ stream.on ~ res:", res.info)
+                    } else {
+                        console.log('--------skip chunk index----------> ', chunk_index)
+                    }
+                    chunk_index++
                     dataArr = []
                     stream.resume()
+
+
                 }
             } else {
                 headerArr = exlArr;
@@ -70,16 +81,20 @@ async function xlsx_fileReading() {
     stream.on('end', async () => {
         console.log("in end process ------->");
         if (dataArr.length) {
-            console.log("🚀 ~ file: stream_and_insertTodb.js:62 ~ stream.on ~ dataArr.length:", dataArr.length)
-            let res = await insertDataMysql(dataArr)
-            console.log("🚀 ~ file: test.js:58 ~ stream.on ~ res:", res.info)
+            if (parseInt(chunk_index) >= parseInt(last_chunk_index)) {
+                console.log("🚀 ~ file: stream_and_insertTodb.js:52 ~ stream.on ~ dataArr.length:", dataArr.length)
+                let res = await insertDataMysql(dataArr)
+                console.log("🚀 ~ file: stream_and_insertTodb.js:53 ~ stream.on ~ res:", res.info)
+            } else {
+                console.log('--------skip chunk index----------> ', chunk_index)
+            }
         }
         console.log("end of process ------------> ");
     })
 }
 
 
-xlsx_fileReading()
+// xlsx_fileReading()
 
 /**
  * This function reads data from an Excel file, processes it in batches of 1000 rows, and inserts it
@@ -191,3 +206,44 @@ async function xlsx_fileReading_4() {
 }
 
 // xlsx_fileReading_4()
+
+
+
+async function csv_reader() {
+    let filePath = `/var/www/html/helo_metal_template/landingpage/upload_files/demo.csv`
+    let flag = false
+    const csvStream = csv({
+        headers: true,
+        ignoreEmpty: this
+    })
+
+    const stream = fs.createReadStream(filePath)
+    .pipe(csvStream)
+        .on('data', async (csvRow) => {
+            console.log("🚀 ~ file: stream_and_insertTodb.js:223 ~ .on ~ csvRow:", csvRow)
+            const csvArray = Object.values(csvRow)
+            console.log("🚀 ~ file: stream_and_insertTodb.js:221 ~ .on ~ csvArray:", csvArray)
+
+            let a = {};
+            if (flag) {
+                if (csvArray.length > 0) {
+                    csvArray.forEach(function (item, index) {
+                        let objKey = headerArray[index];
+                        a[objKey] = item;
+                    });
+                }
+            } else {
+                headerArray = csvArray;
+                flag = true;
+            }
+
+            console.log("🚀 ~ file: stream_and_insertTodb.js:228 ~ .on ~ a:", a)
+
+        })
+        .on('end', () => {
+            console.log('----- in the end process ---------')
+        })
+}
+
+
+csv_reader()
